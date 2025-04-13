ba04cd31137134577029402de10553bb24148c5d
0xcacti <0xcacti@gmail.com>
2025-04-13 02:27:25 -0400

    echo server is working

--- src/main.c
+++ src/main.c
@@ -8,4 +8,28 @@
 #define BUFFER_SIZE 1024
 #define CONNECTION_BACKLOG 10
 
-int main(void) {}
+int main(void) {
+    ipaddr addr = iplocal(NULL, PORT, 0);
+    tcpsock ls = tcplisten(addr, 10);
+    if (!ls) {
+        perror("Can't open listening socket");
+        return 1;
+    }
+
+    while (1) {
+        tcpsock as = tcpaccept(ls, -1);
+        printf("New connection accepted\n");
+        tcpsend(as, "What's your name?\r\n", 19, -1);
+        tcpflush(as, -1);
+
+        char inbuf[256];
+        size_t sz = tcprecvuntil(as, inbuf, sizeof(inbuf), "\r", 1, -1);
+        inbuf[sz - 1] = 0;
+        char outbuf[256];
+        int rc = snprintf(outbuf, sizeof(outbuf), "Hello, %s!\r\n", inbuf);
+        sz = tcpsend(as, outbuf, rc, -1);
+        tcpflush(as, -1);
+        tcpclose(as);
+    }
+    return 0;
+}
