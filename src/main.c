66e96c5d64b79a0fe8c22caec4bbb8f6e90ff593
0xcacti <0xcacti@gmail.com>
2025-04-13 02:46:49 -0400

    using coroutine to handle multiple thins

--- src/main.c
+++ src/main.c
@@ -8,6 +8,34 @@
 #define BUFFER_SIZE 1024
 #define CONNECTION_BACKLOG 10
 
+coroutine void dialogue(tcpsock as) {
+    tcpsend(as, "What's your name?\r\n", 19, -1);
+    if (errno != 0)
+        goto cleanup;
+    tcpflush(as, -1);
+    if (errno != 0)
+        goto cleanup;
+
+    char inbuf[256];
+    size_t sz = tcprecvuntil(as, inbuf, sizeof(inbuf), "\r", 1, -1);
+    if (errno != 0)
+        goto cleanup;
+
+    inbuf[sz - 1] = 0;
+    char outbuf[256];
+    int rc = snprintf(outbuf, sizeof(outbuf), "Hello, %s!\r\n", inbuf);
+
+    sz = tcpsend(as, outbuf, rc, -1);
+    if (errno != 0)
+        goto cleanup;
+    tcpflush(as, -1);
+    if (errno != 0)
+        goto cleanup;
+
+cleanup:
+    tcpclose(as);
+}
+
 int main(void) {
     ipaddr addr = iplocal(NULL, PORT, 0);
     tcpsock ls = tcplisten(addr, 10);
@@ -18,18 +46,9 @@ int main(void) {
 
     while (1) {
         tcpsock as = tcpaccept(ls, -1);
-        printf("New connection accepted\n");
-        tcpsend(as, "What's your name?\r\n", 19, -1);
-        tcpflush(as, -1);
-
-        char inbuf[256];
-        size_t sz = tcprecvuntil(as, inbuf, sizeof(inbuf), "\r", 1, -1);
-        inbuf[sz - 1] = 0;
-        char outbuf[256];
-        int rc = snprintf(outbuf, sizeof(outbuf), "Hello, %s!\r\n", inbuf);
-        sz = tcpsend(as, outbuf, rc, -1);
-        tcpflush(as, -1);
-        tcpclose(as);
+        if (!as)
+            continue;
+        go(dialogue(as));
     }
     return 0;
 }
