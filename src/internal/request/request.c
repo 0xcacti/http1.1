cd0f4e93f6d80da2d75f4585b018a90f8e140623
0xcacti <0xcacti@gmail.com>
2025-05-26 00:51:07 -0400

    lsp

--- src/internal/request/request.c
+++ src/internal/request/request.c
@@ -1,4 +1,14 @@
-#include "internal/request/request.h"
+#include <internal/request/request.h>
+
+typedef struct request_line {
+    char *http_version;
+    char *request_target;
+    char *method;
+} request_line_t;
+
+typedef struct request {
+    request_line_t *line;
+} request_t;
 
 int request_init(void) {
     return 0;
