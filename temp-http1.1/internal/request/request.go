ce1036d5b35f2c0421a1160a92b94b27117ef130
0xcacti <0xcacti@gmail.com>
2025-06-03 14:37:07 -0400

    add parsing ability

--- temp-http1.1/internal/request/request.go
+++ temp-http1.1/internal/request/request.go
@@ -2,13 +2,29 @@ package request
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
 	"unicode"
 )
 
-const bufferSize = 8
+const (
+	BUFFER_SIZE = 8
+	CRLF        = "\r\n"
+)
+
+type requestState int
+
+const (
+	requestStateInitialized requestState = iota
+	requestStateDone
+)
+
+type Request struct {
+	state       requestState
+	RequestLine RequestLine
+}
 
 type RequestLine struct {
 	HttpVersion   string
@@ -16,90 +32,94 @@ type RequestLine struct {
 	Method        string
 }
 
-type Request struct {
-	state       int
-	RequestLine RequestLine
-}
-
-func (r *Request) parse(data []byte) (int, error) {
-	if r.state == 0 {
-		requestLine, bytesRead, err := parseRequestLine(string(data))
-		if err != nil {
-			return 0, fmt.Errorf("failed to parse request line: %w", err)
-		}
-		if bytesRead == 0 {
-			return 0, nil
-		}
-		r.RequestLine = requestLine
-		r.state = 1
-		return bytesRead, nil
-	} else if r.state == 1 {
-		return 0, fmt.Errorf("error: trying to read data in a done state")
-	} else {
-		return 0, fmt.Errorf("unknown state %d", r.state)
-	}
-}
-
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	buf := make([]byte, bufferSize, bufferSize)
+	buf := make([]byte, BUFFER_SIZE, BUFFER_SIZE)
 	readToIndex := 0
-	r := &Request{state: 0}
-	for r.state != 1 {
-		if len(buf) == bufferSize {
-			newBuf := make([]byte, cap(buf)*2, cap(buf)*2)
+	req := &Request{state: requestStateInitialized}
+	for req.state != requestStateDone {
+		if readToIndex >= len(buf) {
+			newBuf := make([]byte, len(buf)*2)
 			copy(newBuf, buf)
 			buf = newBuf
 		}
-		n, err := reader.Read(buf[readToIndex:])
+
+		numBytesRead, err := reader.Read(buf[readToIndex:])
 		if err != nil {
-			if err == io.EOF {
-				r.state = 1
+			if errors.Is(err, io.EOF) {
+				req.state = requestStateDone
 				break
 			}
-			return nil, fmt.Errorf("failed to read from reader: %w", err)
+			return nil, err
 		}
-		readToIndex += n
-		n, err = r.parse(buf[:readToIndex])
+
+		readToIndex += numBytesRead
+		numBytesParsed, err := req.parse(buf[:readToIndex])
 		if err != nil {
-			return nil, fmt.Errorf("failed to parse request: %w", err)
+			return nil, err
 		}
-		newBuf := make([]byte, 0, bufferSize)
-		copy(newBuf, buf[n:])
-		readToIndex -= n
+		copy(buf, buf[numBytesParsed:])
+		readToIndex -= numBytesParsed
 	}
-	return r, nil
+	return req, nil
 }
 
-func parseRequestLine(req string) (RequestLine, int, error) {
-	ctrfIdx := bytes.Index(req, []byte("\r\n"))
-	parts := strings.Split(req, "\r\n")
-	if len(parts) == 0 {
-		return RequestLine{}, 0, nil
+func parseRequestLine(data []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(data, []byte("\r\n"))
+	if idx == -1 {
+		return nil, 0, nil
 	}
 
-	requestLineStr := parts[0]
+	requestLineText := string(data[:idx])
+	requestLine, err := requestLineFromString(requestLineText)
+	if err != nil {
+		return nil, 0, fmt.Errorf("failed to parse request line: %w", err)
+	}
+	return requestLine, idx + len(CRLF), nil
+}
+
+func requestLineFromString(requestLineStr string) (*RequestLine, error) {
+	parts := strings.Split(requestLineStr, " ")
 	requestLineParts := strings.Split(requestLineStr, " ")
-	if len(parts) < 3 {
-		return RequestLine{}, len(parts), fmt.Errorf("Cannot parse request-line")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("Poorly formatted request line: %s", requestLineStr)
 	}
 
 	method := requestLineParts[0]
 	if method != strings.ToUpper(method) || !isAlphabetic(method) {
-		return RequestLine{}, len(parts), fmt.Errorf("Method not capitalized")
+		return nil, fmt.Errorf("Method not capitalized")
 	}
 	requestTarget := requestLineParts[1]
 	httpVersion := requestLineParts[2]
 	if httpVersion != "HTTP/1.1" {
-		return RequestLine{}, len(parts), fmt.Errorf("We only support HTTP/1.1")
+		return nil, fmt.Errorf("We only support HTTP/1.1")
 	}
 	numericVersionOnly := strings.TrimPrefix(httpVersion, "HTTP/")
 
-	return RequestLine{
+	return &RequestLine{
 		HttpVersion:   numericVersionOnly,
 		RequestTarget: requestTarget,
 		Method:        method,
-	}, len(parts), nil
+	}, nil
+}
 
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.state {
+	case requestStateInitialized:
+		requestLine, bytesRead, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		if bytesRead == 0 {
+			return 0, nil
+		}
+		r.RequestLine = *requestLine
+		r.state = 1
+		return bytesRead, nil
+	case requestStateDone:
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	default:
+		return 0, fmt.Errorf("unknown state %d", r.state)
+	}
 }
 
 func isAlphabetic(s string) bool {
