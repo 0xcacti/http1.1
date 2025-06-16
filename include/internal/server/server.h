b856ea843de5ab64b9488ef7a3743ba70ed8e1a0
0xcacti <0xcacti@gmail.com>
2025-06-16 01:37:05 -0400

    start adding requests

--- include/internal/server/server.h
+++ include/internal/server/server.h
@@ -2,12 +2,15 @@
 #define INTERNAL_SERVER_SERVER_H
 
 #include <libmill.h>
+#include <internal/request/request.h>
 
 typedef struct {
     tcpsock listener;
     int closed;
     chan done;
 } server_t;
+
+void handler(request_t *req);
 
 coroutine void listen_routine(server_t *server);
 coroutine void handle_connection(tcpsock conn);
