3092d732085195f1eadf862906fd80923f72c162
0xcacti <0xcacti@gmail.com>
2025-06-25 03:04:23 -0400

    wow C is hard

--- src/httpserver/main.c
+++ src/httpserver/main.c
@@ -74,64 +74,75 @@ void handle_200(response_writer_t *w, request_t *req) {
     write_body(w, body, len_body);
 }
 
-// static size_t header_cb(char *buf, size_t size, size_t nmemb, void *userdata) {
-//     (void)buf;
-//     proxy_ctx_t *ctx = userdata;
-//     size_t len = size * nmemb;
-//
-//     if (!ctx->called) {
-//         long status = 0;
-//         curl_easy_getinfo(ctx->curl, CURLINFO_RESPONSE_CODE, &status);
-//
-//         if (write_status_line(ctx->w, (int)status) < 0) {
-//             handle_500(ctx->w, ctx->req);
-//             return 0;
-//         }
-//     }
-//
-//     headers_t *hdrs = get_default_headers(0);
-//     headers_delete(hdrs, "Content-Length");
-//     headers_set(hdrs, "Transfer-Encoding", "chunked");
-//     if (write_headers(ctx->w, hdrs) < 0) {
-//         handle_500(ctx->w, ctx->req);
-//         free_headers(hdrs);
-//         return 0;
-//     }
-//     free_headers(hdrs);
-//     ctx->called = 1;
-//
-//     return len;
-// }
-//
-// void forward_proxy(response_writer_t *w, request_t *req, const char *target) {
-//     CURL *curl = curl_easy_init();
-//     if (!curl) {
-//         handle_500(w, req);
-//         return;
-//     }
-//
-//     char *url = malloc(strlen("https://httpbin.org") + strlen(target) + 1);
-//     sprintf(url, "https://httpbin.org%s", target);
-//
-//     proxy_ctx_t ctx = {.w = w, .req = req, .curl = curl, .called = 0};
-//
-//     curl_easy_setopt(curl, CURLOPT_URL, url);
-//     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-//     curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_cb);
-//     curl_easy_setopt(curl, CURLOPT_HEADERDATA, &ctx);
-//     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
-//     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ctx);
-//
-//     if (curl_easy_perform(curl) != CURLE_OK) {
-//         handle_500(w, req);
-//     } else {
-//         if (write_chunked_body_done(w) < 0) {
-//             handle_500(w, req);
-//         }
-//     }
-//     free(url);
-//     curl_easy_cleanup(curl);
-// }
+void forward_proxy(response_writer_t *w, request_t *req, const char *target) {
+    ipaddr addr = ipremote("httpbin.org", 80, IPADDR_IPV4, -1);
+    tcpsock sock = tcpconnect(addr, -1);
+    if (sock == NULL) {
+        handle_500(w, req);
+        return;
+    }
+
+    char reqbuf[512];
+    int req_len = snprintf(reqbuf, sizeof(reqbuf),
+                           "GET %s HTTP/1.1\r\n"
+                           "Host: httpbin.org\r\n"
+                           "Connection: close\r\n"
+                           "\r\n",
+                           target);
+    if (tcpsend(sock, reqbuf, req_len, -1) != (size_t)req_len) {
+        tcpclose(sock);
+        handle_500(w, req);
+        return;
+    }
+
+    char buf[4096];
+    size_t buf_len = 0;
+    int headers_done = 0;
+
+    while (1) {
+        ssize_t r = tcprecv(sock, buf + buf_len, sizeof(buf) - buf_len - 1, -1);
+        if (r < 0) {
+            tcpclose(sock);
+            handle_500(w, req);
+            return;
+        }
+        if (r == 0) {
+            break;
+        }
+        buf_len += (size_t)r;
+        if (!headers_done) {
+            char *eoh = memmem(buf, buf_len, "\r\n\r\n", 4);
+            if (eoh == NULL) {
+                if (buf_len == sizeof(buf)) {
+                    tcpclose(sock);
+                    handle_500(w, req);
+                    return;
+                }
+                continue;
+            }
+            write_status_line(w, RESPONSE_STATUS_OK);
+            headers_t *headers = get_default_headers(0);
+            headers_delete(headers, "Content-Length");
+            headers_set(headers, "Transfer-Encoding", "chunked");
+            write_headers(w, headers);
+            free_headers(headers);
+
+            size_t header_len = (eoh + 4) - buf;
+            size_t body_len = buf_len - header_len;
+            if (body_len > 0) {
+                write_chunked_body(w, buf + header_len, body_len);
+            }
+            memmove(buf, buf + header_len, body_len);
+            buf_len = body_len;
+            headers_done = 1;
+        } else {
+            write_chunked_body(w, buf, buf_len);
+            buf_len = 0;
+        }
+    }
+    write_chunked_body_done(w);
+    tcpclose(sock);
+}
 
 void handle(response_writer_t *w, request_t *req) {
     char *method = req->request_line->request_target;
